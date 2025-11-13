dd45d6e4d99d256b50b6efaec09ee92985d14fd1
Mohan <sunnyjaya2203@gmail.com>
2025-11-13 12:06:16 +0530

    add oops_10.py to implement a BankAccount class with deposit and withdrawal methods

---
+++ Python/oops_10.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, account_holder, balance=0):
+        self.account_holder = account_holder
+        self.__balance = balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance +=amount
+            print(f"Deposited: {amount}. New Balance: {self.__balance}")
+        else:
+            print("Deposit amount must be positive.")
+    def withdraw(self, amount):
+        if 0 < amount < self.__balance:
+            self.__balance -= amount
+            print(f"Withdrew: {amount}. New Balance: {self.__balance}")
+        else:
+            print("Insufficient funds or invalid withdrawal amount.")
+    def get_balance(self):
+        return self.__balance
+
+acc1=BankAccount("Mohan", 1000)
+acc1.deposit(500)
+acc1.withdraw(20)
+acc1.get_balance()
