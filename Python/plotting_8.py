98781761ae03e40342d3998f23ab8ef72d3fd8ae
Mohan <sunnyjaya2203@gmail.com>
2025-11-26 21:12:06 +0530

    change line plot to bar chart for sales per category visualization

--- Python/plotting_8.py
+++ Python/plotting_8.py
@@ -12,7 +12,7 @@ sales_df=pd.DataFrame({'product_category':['Electronics', 'Clothing', 'Groceries
                           'sales_amount':[150000, 80000, 120000, 60000, 40000]})
 total_sales_df=sales_df['sales_amount'].sum()
 
-plt.plot(sales_df['product_category'],sales_df['sales_amount'])
+plt.bar(sales_df['product_category'],sales_df['sales_amount'])
 plt.title('Sales per Category')
 plt.xlabel('Product Category')
 plt.ylabel('Sales Amount')
